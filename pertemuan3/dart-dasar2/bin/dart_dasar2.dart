c4f080715dbbad912236ec3b53695a2b54349a7f
Ghetsa Ramadhani <ghetsa.arryanti@gmail.com>
2025-10-01 12:01:59 +0700

    add: pertemuan 3

---
+++ pertemuan3/dart-dasar2/bin/dart_dasar2.dart
@@ -0,0 +1,5 @@
+import 'package:dart_dasar2/dart_dasar2.dart' as dart_dasar2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_dasar2.calculate()}!');
+}
---
+++ pertemuan4/dart-dasar3/bin/dart_dasar3.dart
@@ -0,0 +1,5 @@
+import 'package:dart_dasar3/dart_dasar3.dart' as dart_dasar3;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_dasar3.calculate()}!');
+}
